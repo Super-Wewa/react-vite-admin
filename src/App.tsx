5e39d9a580b008e26bc8d857fb9017aad8bfb383
theblackman <paskasonga@gmail.com>
2025-03-08 17:44:17 +0100

    project initialisation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className='w-full min-h-screen flex items-center justify-center'>
+      <p>
+        Super Wewa Admin Pannel
+      </p>
+    </div>
+  )
+}
+
+export default App
